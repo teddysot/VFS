606db1b7a42a0db1e47cfd9c600bc065d0893a10
Saharat Nasahachart <earth_sot@hotmail.com>
2019-06-04 19:29:55 -0700

    Added RP Orbitting and MatchingStars

---
+++ RapidPrototype/OrbitalPath/Assets/Scripts/OrbitMotion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitMotion : MonoBehaviour
+{
+    [SerializeField] private Transform orbitingObject;
+    [SerializeField] private GameObject orbitPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Unity2/ThirdPersonShooter/Assets/Scripts/PlayerController.cs
+++ Unity2/ThirdPersonShooter/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : Unit
 
     [SerializeField]
     private float _SprintSpeedMult = 5f;
- 
+
     [SerializeField]
     private float _JumpSpeed = 12f;
 
@@ -21,10 +21,13 @@ public class PlayerController : Unit
     private Transform _CameraPivot;
 
     private Transform _PlayerCamera;
-    private float _SpeedMult;
+    private float _SpeedMult = 1.0f;
     private float _XInput;
     private float _ZInput;
     private bool _JumpPressed;
+    private bool _SideWalk = false;
+    private bool _BackWalk = false;
+    private bool _ForwardWalk = false;
 
     public override void UnitAwake()
     {
@@ -36,7 +39,7 @@ public class PlayerController : Unit
         Cursor.lockState = CursorLockMode.Locked;
         UpdateCameraZoom();
 
-        if(IsAlive == false) return;
+        if (IsAlive == false) return;
 
         ReadMoveInputs();
         SetAnimValues();
@@ -46,12 +49,12 @@ public class PlayerController : Unit
 
     private void ShootLasers()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             var ray = new Ray(_PlayerCamera.position, _PlayerCamera.forward);
-            if(Physics.Raycast(ray, out var hit))
+            if (Physics.Raycast(ray, out var hit))
             {
-                if(CanUnitSee(hit.point, hit.transform))
+                if (CanUnitSee(hit.point, hit.transform))
                 {
                     ShootLasersFromEyes(hit.point, hit.transform);
                 }
@@ -61,19 +64,57 @@ public class PlayerController : Unit
 
     private void FixedUpdate()
     {
-        if(IsAlive == false) return;
-        
+        if (IsAlive == false) return;
+
         ApplyMovePhysics();
     }
 
     private void ApplyMovePhysics()
     {
         var newVel = new Vector3(_XInput, 0f, _ZInput) * _MoveSpeed * _SpeedMult;
+
         newVel = transform.TransformVector(newVel);
         newVel.y = _JumpPressed ? _JumpSpeed : _RB.velocity.y;
         _RB.velocity = newVel;
 
         _JumpPressed = false;
+    }
+
+    private void CheckMovement()
+    {
+        if (_XInput < 0.0f || _XInput > 0.0f)
+        {
+            _SideWalk = true;
+        }
+        else if (_ZInput < 0.0f)
+        {
+            _BackWalk = true;
+        }
+        else if (_ZInput > 0.0f)
+        {
+            _ForwardWalk = true;
+        }
+        else
+        {
+            _ForwardWalk = false;
+            _BackWalk = false;
+            _SideWalk = false;
+        }
+    }
+
+    private float SetSpeedMult()
+    {
+        float speed = 1.0f;
+        if (_SideWalk && _ForwardWalk == false)
+        {
+            speed = 0.6f;
+        }
+        else if (_BackWalk || _SideWalk && _BackWalk)
+        {
+            speed = 0.3f;
+        }
+
+        return speed;
     }
 
     private void UpdateCameraZoom()
@@ -88,15 +129,21 @@ public class PlayerController : Unit
     {
         var mouseX = Input.GetAxisRaw("Mouse X");
         var mouseY = Input.GetAxisRaw("Mouse Y");
+
         transform.Rotate(0f, mouseX, 0f);
-        _CameraPivot.Rotate(-mouseY, 0f, 0f);
+
+        mouseY = Mathf.Clamp(mouseY, -90.0f, 90.0f);
+        _CameraPivot.localRotation = Quaternion.Euler(-mouseY, 0.0f, 0.0f);
     }
 
     private void ReadMoveInputs()
     {
         _XInput = Input.GetAxis("Horizontal");
         _ZInput = Input.GetAxis("Vertical");
-        _SpeedMult = Input.GetKey(_SprintKey) ? _SprintSpeedMult : 1f;
+        CheckMovement();
+        var speedDefault = SetSpeedMult();
+        Debug.Log(speedDefault);
+        _SpeedMult = Input.GetKey(_SprintKey) ? _SprintSpeedMult : speedDefault;
         if (Input.GetKeyDown(_JumpKey))
         {
             _JumpPressed = true;
