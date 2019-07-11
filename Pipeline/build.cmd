856e727014362b5515d0d7218a873b3deedc1742
Teddy <earth_sot@hotmail.com>
2019-07-10 20:35:22 -0700

    Updated Pipeline Build Batch

--- Pipeline/build.cmd
+++ Pipeline/build.cmd
@@ -1,20 +1,35 @@
-@echo on
-if "%1"=="" goto help
-if "%1"=="-art" set BUILD_TYPE=ART
+@echo off
+echo [[95m=============== [91mAUTOMATED UNITY BUILD[0m [95m==============[0m]
+echo [[95m=============== [91mTEAM DIAMOND DONKEYS[0m [95m===============[0m]
+echo [[95m=============== [91mVANCOUVER FILM SCHOOL[0m [95m==============[0m]
+echo [[95m================== [91mGD54 and PG15[0m [95m===================[0m]
 
-REM This is a comment
+timeout 3 > NUL
 
-:build_code
-type errors.log > another_file.log > build.log
-bin\grep -n "error" another_file.log / bin\gawk "{ print \"The error is \" $1 $3 }" >> build.log
-del another_file.log
-%BUILD_SYS myapp.sln
-goto end
+REM Initialize Paths
+set CURRENT_DIR=%cd%
+set WORKSPACE=C:\Users\pg15teddy\Perforce\Unity\...
+set EDITOR="C:\Program Files\Unity\Hub\Editor\2019.1.8f1\Editor\Unity.exe"
+set PROJECT_DIR=C:\Users\pg15teddy\Perforce\Unity\Test\
 
-:help
-@echo .         Help or the build script
-@echo .         Usage:  build [date-string]
-dir %USERPROFILE%
+echo .
+echo .
+echo [[95m========== [92mPERFORCE SYNCING [95m==========[0m]
+timeout 3 > NUL
+p4 set P4PORT=perforce:1666
+p4 set P4USER=pg15teddy
+p4 set P4CLIENT=pg15teddy_SCT201-03
+set P4PASSWD=0BD45470B11E9CD84AFE28387BD98B48
+p4 set P4IGNORE=.p4ignore.txt
+p4 sync -f %WORKSPACE%
+echo [[95m========== [92mPERFORCE SYNCED [95m===========[0m]
+echo .
+echo .
 
-:end
-set BUILD_TYPE=
+echo [[95m========== [93mUNITY BUILDING [95m============[0m]
+%EDITOR% -quit -batchmode -logFile .\Builds\build.log -projectPath %PROJECT_DIR% -buildWindows64Player %CURRENT_DIR%\Builds\Test.exe
+echo [[95m========== [93mUNITY BUILDED [95m=============[0m]
+
+echo [96m
+pause
+echo [0m
